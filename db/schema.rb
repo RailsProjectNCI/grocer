39c6bb057db789bc71e2d5c0a131d3429539933a
dunnec <conordunne4@gmail.com>
2018-04-07 09:40:33 +0100

    Added fname, lname, dob to User table and reflected this on forms

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180407072926) do
+ActiveRecord::Schema.define(version: 20180407083006) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
@@ -34,6 +34,9 @@ ActiveRecord::Schema.define(version: 20180407072926) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "fname"
+    t.string "lname"
+    t.date "dob"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
