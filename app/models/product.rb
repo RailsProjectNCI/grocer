32be2a9976f3404f4aa5e966aa3103f7257b1781
dunnec <conordunne4@gmail.com>
2018-04-07 18:37:17 +0100

    Possible fix to reload database. Removed password reste token

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
 	belongs_to :user
 	belongs_to :category
+	has_many :rating
 
 	def self.search(search)
 		where("title ILIKE ?", "%#{search}%")
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
 
   get 'pages/delivery'
 
-  resources :products
+  resources :products do
+  	resources :ratings
+  end 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'pages#home'
